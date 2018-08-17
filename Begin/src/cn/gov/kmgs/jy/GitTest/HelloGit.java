c91d9bdec462a3bb97660b7c5326a5f4c4ca2ca0
927535013 <927535013@qq.com>
2018-08-17 11:04:18 +0800

    v0.01

---
+++ Begin/src/cn/gov/kmgs/jy/GitTest/HelloGit.java
@@ -0,0 +1,10 @@
+package cn.gov.kmgs.jy.GitTest;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
